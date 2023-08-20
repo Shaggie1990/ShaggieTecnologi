c50614c8bb934024c4b0db2ffa9c6e82939100e5
leandro1990 <gomez.leandro1990@gmail.com>
2023-08-20 16:17:51 -0300

    [feature] - create stack auth and implement in root navigator

--- src/constants/firebase/index.js
+++ src/constants/firebase/index.js
@@ -1,2 +1,4 @@
 export const URL_BASE_FIREBASE_REALTIME_DATABASE = 
         "https://shaggietecnologi-default-rtdb.firebaseio.com";
+
+export const FIREBASE_API_KEY = "AIzaSyCIDHzUX1fQsow1Hp5vZ8x7zPIzlrLZidc";
--- src/navigations/index.jsx
+++ src/navigations/index.jsx
@@ -1,12 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { useState } from 'react';
 
 import TabsNavigator from './tabs';
+import AuthNavigator from './auth';
 
 function RootNavigator() {
+    const [userId, setUserId] = useState(null);
     return (
-        <NavigationContainer>
-            <TabsNavigator />
-        </NavigationContainer>
+        <NavigationContainer>{userId ? <TabsNavigator /> : <AuthNavigator />}</NavigationContainer>
     );
 }
 

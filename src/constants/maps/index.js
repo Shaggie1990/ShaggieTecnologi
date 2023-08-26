76d5808e7cce36128c6ddaec80493a4f5be38bde
leandro1990 <gomez.leandro1990@gmail.com>
2023-08-26 00:07:08 -0300

    [feature] - update address in RTDB , get geolocation in address screen

--- src/constants/maps/index.js
+++ src/constants/maps/index.js
@@ -1,3 +1,5 @@
 export const GOOGLE_API_KEY = 'AIzaSyAmFxma_6wc8fIIwU-3enHUgj125TGP8BE';
 export const URL_MAPS = ({ lat, lng, zoom }) =>
     `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
+
+export const URL_BASE_GEOCODING = 'https://maps.googleapis.com/maps/api/geocode';
